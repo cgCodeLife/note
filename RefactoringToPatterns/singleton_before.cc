45b5d2a7da65a8208c64a8da5641c3f452682603
caoge <caoge@bytedance.com>
2021-06-19 14:34:16 +0800

    add singleton

--- RefactoringToPatterns/singleton_before.cc
+++ RefactoringToPatterns/singleton_before.cc
@@ -1,11 +1,14 @@
 
 #include <iostream>
+#include <memory>
 #include <string>
-#include <vetor>
+#include <vector>
 
 class HitStayResponse {
 public:
-    std::string ToString() {
+    virtual ~HitStayResponse() {}
+
+    virtual std::string ToString() {
         return "HitStayResponse";
     }
 };
@@ -24,6 +27,44 @@ public:
     }
 };
 
-class Blackjack {
-    public:
+class Console {
+public:
+    static HitStayResponse* ObtainHitStayResponse() {
+        return hit_stay_response_;
+    }
+
+    static void SetPlayerResponse(HitStayResponse* hit_stay_response) {
+        hit_stay_response_ = hit_stay_response;
+    }
+
+private:
+    static HitStayResponse* hit_stay_response_;
 };
+
+HitStayResponse* Console::hit_stay_response_ = nullptr;
+
+class Blackjack {
+public:
+    void Play() {
+        auto* hitStayResponse = Console::ObtainHitStayResponse();
+        std::cout << hitStayResponse->ToString() << std::endl;
+    }
+};
+
+int main() {
+    {
+        Blackjack black_jack;
+        std::unique_ptr<HitStayResponse> hit_stay_response;
+        hit_stay_response.reset(new TestHitStayResponse());
+        Console::SetPlayerResponse(hit_stay_response.get());
+        black_jack.Play();
+    }
+
+    {
+        Blackjack black_jack;
+        std::unique_ptr<HitStayResponse> hit_stay_response;
+        hit_stay_response.reset(new TestAlwaysHitResponse());
+        Console::SetPlayerResponse(hit_stay_response.get());
+        black_jack.Play();
+    }
+} 
